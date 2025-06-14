492d0587d5b4806cb91a7252bb78dea61454fdb9
Alberto SanchezS <albertosanchez05@gmail.com>
2025-06-13 20:18:27 -0500

    chore: add dependency injection

--- ACME.LearningCenterPlatform.API/Profiles/Domain/Model/Aggregates/Profile.cs
+++ ACME.LearningCenterPlatform.API/Profiles/Domain/Model/Aggregates/Profile.cs
@@ -1,5 +1,4 @@
 using ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Commands;
-using ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Entities;
 using ACME.LearningCenterPlatform.API.Profiles.Domain.Model.ValueObjects;
 
 namespace ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Aggregates;
@@ -34,7 +33,5 @@ public partial class Profile
             command.Country
         );
     }
-    
-    public List<Order> Orders { get; set; }
     
 }
--- ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,5 +1,3 @@
-using ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Aggregates;
-using ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Entities;
 using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
@@ -21,16 +19,6 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-
-        builder.Entity<Profile>().HasKey(p => p.Id);
-        builder.Entity<Order>().HasKey(p => p.Id);
-
-        builder.Entity<Profile>()
-            .HasMany(p => p.Orders)
-            .WithOne(o => o.Profile)
-            .HasForeignKey(o => o.ProfileId)
-            .OnDelete(DeleteBehavior.Restrict);
-        
         builder.UseSnakeCaseNamingConvention();
     }
 }
