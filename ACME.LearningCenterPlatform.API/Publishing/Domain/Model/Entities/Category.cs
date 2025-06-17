c46b2e62cc630b14f97a167f5deb29f1371b01dc
Alberto SanchezS <albertosanchez05@gmail.com>
2025-06-16 21:54:57 -0500

    feat: add publishing entities

--- ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
@@ -4,9 +4,14 @@ public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; }
-
+    
     public Category()
     {
         Name = string.Empty;
+    }
+
+    public Category(string name)
+    {
+        Name = name;
     }
 }
--- ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/ReadableContentAsset.cs
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/ReadableContentAsset.cs
@@ -1,6 +1,19 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
 namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
 
-public class ReadableContentAsset
+public class ReadableContentAsset : Asset
 {
-    
+    public ReadableContentAsset() : base(EAssetType.ReadableContentItem)
+    {
+        ReadableContent = string.Empty;
+    }
+
+    public ReadableContentAsset(string content) : base(EAssetType.ReadableContentItem)
+    {
+        ReadableContent = content;
+    }    
+    public string ReadableContent { get; set; }
+    public override bool Readable => true;
+    public override bool Viewable => false;
 }
